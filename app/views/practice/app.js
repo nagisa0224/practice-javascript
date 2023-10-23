1677fccdb756326cad15f5fa0918e3dd5fd78188
nagisa0224 <lefretshurk@gmail.com>
2023-10-23 14:50:33 +0000

    break

--- app/views/practice/app.js
+++ app/views/practice/app.js
@@ -1,1 +1,26 @@
 alert('Hello World');
+
+// 整数を代入する
+let int1 = 1;
+
+// 負数を代入する
+let int2 = -10;
+
+// 小数点を代入する
+let float1 = 3.14;
+
+// 文字列を代入する
+let str1 = 'JavaScriptを覚えよう';
+
+
+// 足し算
+alert(4 + 3);
+
+// 引き算
+alert(8 - 5);
+
+// 掛け算
+alert(2 * 6);
+
+// 割り算
+alert(10 / 2);
