1e4634216fad8bf745ef44214538d61f2f010a23
nagisa0224 <lefretshurk@gmail.com>
2023-11-06 14:31:21 +0000

    break

---
+++ app/views/practice/practice.java
@@ -0,0 +1,15 @@
+System.out.println("Hello World");
+
+
+int number = 3;
+System.out.println(number);
+
+String name = "Sato";
+System.out.println(name);
+
+System.out.println(number + 3);
+
+System.out.println("私の名前は" + name + "です");
+
+// 変数の内容を上書きする場合は 変数名 = 内容
+name = "田中";
