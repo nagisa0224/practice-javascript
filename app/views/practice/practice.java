23980240d7e5400c87d192d40f0c6351fd5c9d13
nagisa0224 <lefretshurk@gmail.com>
2023-11-08 14:27:24 +0000

    break

--- app/views/practice/practice.java
+++ app/views/practice/practice.java
@@ -13,3 +13,27 @@ System.out.println("私の名前は" + name + "です");
 
 // 変数の内容を上書きする場合は 変数名 = 内容
 name = "田中";
+
+
+// switch文
+int n = 1;
+switch (n) {
+  case 1 :
+    System.out.println("大吉です");
+    break;
+  case 2 :
+    System.out.println("吉です");
+    break;
+}
+
+// breakはswitch文を終了する命令に使用。ないと次のcaseも実行されてしまいます！
+
+int n = 2;
+switch (n) {
+  case 1 :
+    System.out.println("クレープです");
+    break;
+  case 2 :
+    System.out.println("パフェです");
+    break;
+}
