01b40c805c3fb313f1d685c4d38ce1afb69d1f56
nagisa0224 <lefretshurk@gmail.com>
2023-11-15 03:36:22 +0000

    break

---
+++ app/views/practice/mirror.java
@@ -0,0 +1,53 @@
+public class Mirror {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
+
+
+public class Mirror {
+  public static void main(String[] args) {
+    System.out.println("31 + 31の計算をしてください");
+    System.out.println(31 + 31);
+    System.out.println(6 * 6 * 3.14);
+    System.out.println("答えは" + 64);
+  }
+}
+
+
+public class Mirror {
+  public static void main(String[] argse) {
+    int x;
+    x = 6;
+    System.out.println(x * x * 3.14);
+  }
+}
+
+
+// 行末までコメントアウト //
+
+/* 複数行
+   コメントできるよ */
+
+
+public class Mirror {
+  public static void main(String[] args) {
+    int age = 20;
+    System.out.println("私の年齢は" + age);
+    age = 31;
+    System.out.println("本当の年齢は" + age);
+  }
+}
+
+// 上の段のageには20
+// 下の段のageには31が代入されている(変数の上書き)
+
+
+public class Mirror {
+  public static void main(String[] args) {
+    final double PI = 3.14;
+    int pie = 5;
+    System.out.println("半径" + pie + "cmのパイの面積は、");
+    System.out.println(pie * pie * PI);
+  }
+}
