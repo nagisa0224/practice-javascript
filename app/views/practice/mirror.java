a5ffb0c03fdaf20c74d892e70e66c7875e3ed11b
nagisa0224 <lefretshurk@gmail.com>
2023-11-22 14:52:52 +0000

    break

--- app/views/practice/mirror.java
+++ app/views/practice/mirror.java
@@ -144,3 +144,15 @@ public class Mirror {
     System.out.println(msg);
   }
 }
+
+
+public class Mirror {
+  public static void main(String[] args) {
+    String name = "すがわら";
+    System.out.print("私の名前は");
+    System.out.print(name);
+    System.out.print("です");
+  }
+}
+
+// 上記のSystem.out.print
