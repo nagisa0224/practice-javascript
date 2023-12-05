bb2e21c6dc3d1603ed4cc448e8ab7119ca75664a
nagisa0224 <lefretshurk@gmail.com>
2023-12-05 08:40:05 +0000

    break

--- app/views/practice/mirror.java
+++ app/views/practice/mirror.java
@@ -376,4 +376,64 @@ public class Problem5 {
     }
 }
 
+public class SumCalculator {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int result = add(5, 7);
+        System.out.println("Sum: " + result);
+    }
+}
+
+public class NumberPrinter {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+public class EvenOddChecker {
+    public static String checkEvenOdd(int number) {
+        return (number % 2 == 0) ? "Even" : "Odd";
+    }
+
+    public static void main(String[] args) {
+        int numToCheck = 15;
+        System.out.println(numToCheck + " is " + checkEvenOdd(numToCheck));
+    }
+}
+
+public class ReverseString {
+    public static String reverse(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        String original = "Hello, World!";
+        String reversed = reverse(original);
+        System.out.println("Original: " + original);
+        System.out.println("Reversed: " + reversed);
+    }
+}
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
+
+
 }
