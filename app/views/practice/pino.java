8a2ce37137067e2a9a977c2a5e8da1ca92bca09f
nagisa0224 <lefretshurk@gmail.com>
2023-12-07 13:06:46 +0000

    break

--- app/views/practice/pino.java
+++ app/views/practice/pino.java
@@ -57,4 +57,51 @@ public class FizzBuzz {
         }
     }
 }
+
+public class PrimeNumbers {
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 50; i++) {
+            if (isPrime(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+}
+
+public class PrimeNumbers {
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public static void main(String[] args) {
+        for (int i = 1; i <= 50; i++) {
+            if (isPrime(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+}
+
+
+
 }
