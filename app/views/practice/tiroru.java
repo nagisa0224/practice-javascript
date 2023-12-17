28162eb280bdd16572d4d21112992a3490f41f3a
nagisa0224 <lefretshurk@gmail.com>
2023-12-17 14:21:45 +0000

    break

--- app/views/practice/tiroru.java
+++ app/views/practice/tiroru.java
@@ -22,3 +22,23 @@ public class Tiroru {
 
 
 import java.util.*;
+public class Tiroru {
+  public static void main (String [] args) {
+    Scanner sc = new Scanner (System.in);
+    
+    int money = sc.nextInt();
+    int n = sc.nextInt();
+    int point = 0;
+    
+    for (int i = 0; i < n; i++) {
+      int uc = sc.nextInt();
+      if (point >= uc) {
+        point -= uc;
+      } else {
+        money = money - uc;
+        point += uc / 10;
+      }
+      System.out.println(money + " " + point);
+    }
+  }
+}
