183b02ea82892b910cffd861aa74835404b978b8
nagisa0224 <lefretshurk@gmail.com>
2023-12-17 14:15:55 +0000

    breal

---
+++ app/views/practice/tiroru.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Tiroru {
+  public static void main (String [] args) {
+    Scanner sc = new Scanner (System.in);
+    
+    int money = sc.nextInt();
+    int n = sc.nextInt();
+    int point = 0;
+    
+    for (int i = 0; i < n; i++) {
+      int uc = sc.nextInt();
+      if (point >= uc) {
+        point -= uc;
+      } else {
+        money = money - uc;
+        point += uc / 10;
+      }
+      System.out.println(money + " " + point);
+    }
+  }
+}
+
+
+import java.util.*;
